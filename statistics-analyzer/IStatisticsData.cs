ba00f69df783a577cc0baa2732fe0c82d0e8a740
Brutkesnok <106821387+Brutkesnok@users.noreply.github.com>
2023-05-18 06:41:12 +0300

    Adding the 'IStatisticsData' interface and implementing the basic GUI functionality

---
+++ statistics-analyzer/IStatisticsData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace statistics_analyzer
+{
+    public interface IStatisticsData
+    {
+        bool LoadData(string filePath);
+
+        string[] GetFields();
+
+        List<string[]> GetEntries();
+
+        string GetSummary();
+    }
+}
--- statistics-analyzer/MainForm.cs
+++ statistics-analyzer/MainForm.cs
@@ -23,9 +23,21 @@ namespace statistics_analyzer
             MessageBox.Show(message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
 
-        private void UpdateInfo()
+        private void UpdateInfo(IStatisticsData statisticsData)
         {
-
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() != DialogResult.OK)
+                return;
+            if (statisticsData.LoadData(openFileDialog.FileName))
+            {
+                UpdateTable(statisticsData.GetFields(), statisticsData.GetEntries());
+                UpdateChart(statisticsData.GetFields(), statisticsData.GetEntries());
+                UpdateSummary(statisticsData.GetSummary());
+            }
+            else
+            {
+                ShowError("Invalid data!");
+            }
         }
 
         private void apartmentPricesToolStripMenuItem_Click(object sender, EventArgs e)
@@ -38,19 +50,40 @@ namespace statistics_analyzer
 
         }
 
-        private void UpdateTable()
+        private void UpdateTable(string[] headers, List<string[]> data)
         {
-
+            statisticsDataGridView.ColumnCount = headers.Length;
+            statisticsDataGridView.Rows.Clear();
+            for (int i = 0; i < headers.Length; ++i)
+            {
+                statisticsDataGridView.Columns[i].Name = headers[i];
+            }
+            for (int i = 0; i < data.Count; ++i)
+            {
+                statisticsDataGridView.Rows.Add(data[i]);
+            }
         }
 
-        private void UpdateChart()
+        private void UpdateChart(string[] fields, List<string[]> data)
         {
+            timelineChart.Series.Clear();
+            timelineChart.ChartAreas.Clear();
+            timelineChart.ChartAreas.Add(new ChartArea());
 
+            for (int j = 1; j < fields.Length; ++j)
+            {
+                Series series = new Series(fields[j]);
+                series.ChartType = SeriesChartType.Line;
+                series.BorderWidth = 2;
+                for (int i = 0; i < data.Count; ++i)
+                    series.Points.AddXY(Convert.ToInt32(data[i][0]), Convert.ToInt32(data[i][j]));
+                timelineChart.Series.Add(series);
+            }
         }
 
-        private void UpdateSummary()
+        private void UpdateSummary(string summary)
         {
-
+            summaryRichTextBox.Text = summary;
         }
     }
 }
