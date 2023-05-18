72eef73e265e4535badab7c2d7292a2c92816204
Brutkesnok <106821387+Brutkesnok@users.noreply.github.com>
2023-05-18 06:38:25 +0300

    GUI created

---
+++ statistics-analyzer/MainForm.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace statistics_analyzer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.openFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.statisticsDataGridView = new System.Windows.Forms.DataGridView();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.timelineChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.summaryRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.apartmentPricesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.medianSalariesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.statisticsDataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.timelineChart)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFileToolStripMenuItem});
+            resources.ApplyResources(this.menuStrip1, "menuStrip1");
+            this.menuStrip1.Name = "menuStrip1";
+            // 
+            // openFileToolStripMenuItem
+            // 
+            this.openFileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.apartmentPricesToolStripMenuItem,
+            this.medianSalariesToolStripMenuItem});
+            this.openFileToolStripMenuItem.Name = "openFileToolStripMenuItem";
+            resources.ApplyResources(this.openFileToolStripMenuItem, "openFileToolStripMenuItem");
+            // 
+            // splitContainer1
+            // 
+            resources.ApplyResources(this.splitContainer1, "splitContainer1");
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.statisticsDataGridView);
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            // 
+            // statisticsDataGridView
+            // 
+            this.statisticsDataGridView.AllowUserToAddRows = false;
+            this.statisticsDataGridView.AllowUserToDeleteRows = false;
+            this.statisticsDataGridView.AllowUserToResizeColumns = false;
+            this.statisticsDataGridView.AllowUserToResizeRows = false;
+            this.statisticsDataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.statisticsDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            resources.ApplyResources(this.statisticsDataGridView, "statisticsDataGridView");
+            this.statisticsDataGridView.Name = "statisticsDataGridView";
+            this.statisticsDataGridView.ReadOnly = true;
+            this.statisticsDataGridView.RowHeadersVisible = false;
+            this.statisticsDataGridView.RowTemplate.Height = 28;
+            // 
+            // splitContainer2
+            // 
+            resources.ApplyResources(this.splitContainer2, "splitContainer2");
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.groupBox2);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.groupBox3);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.timelineChart);
+            resources.ApplyResources(this.groupBox2, "groupBox2");
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.TabStop = false;
+            // 
+            // timelineChart
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.timelineChart.ChartAreas.Add(chartArea2);
+            resources.ApplyResources(this.timelineChart, "timelineChart");
+            legend2.Name = "Legend1";
+            this.timelineChart.Legends.Add(legend2);
+            this.timelineChart.Name = "timelineChart";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.timelineChart.Series.Add(series2);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.summaryRichTextBox);
+            resources.ApplyResources(this.groupBox3, "groupBox3");
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.TabStop = false;
+            // 
+            // summaryRichTextBox
+            // 
+            this.summaryRichTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            resources.ApplyResources(this.summaryRichTextBox, "summaryRichTextBox");
+            this.summaryRichTextBox.Name = "summaryRichTextBox";
+            this.summaryRichTextBox.ReadOnly = true;
+            // 
+            // apartmentPricesToolStripMenuItem
+            // 
+            this.apartmentPricesToolStripMenuItem.Name = "apartmentPricesToolStripMenuItem";
+            resources.ApplyResources(this.apartmentPricesToolStripMenuItem, "apartmentPricesToolStripMenuItem");
+            this.apartmentPricesToolStripMenuItem.Click += new System.EventHandler(this.apartmentPricesToolStripMenuItem_Click);
+            // 
+            // medianSalariesToolStripMenuItem
+            // 
+            this.medianSalariesToolStripMenuItem.Name = "medianSalariesToolStripMenuItem";
+            resources.ApplyResources(this.medianSalariesToolStripMenuItem, "medianSalariesToolStripMenuItem");
+            this.medianSalariesToolStripMenuItem.Click += new System.EventHandler(this.medianSalariesToolStripMenuItem_Click);
+            // 
+            // MainForm
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.statisticsDataGridView)).EndInit();
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.timelineChart)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem openFileToolStripMenuItem;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart timelineChart;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RichTextBox summaryRichTextBox;
+        private System.Windows.Forms.DataGridView statisticsDataGridView;
+        private System.Windows.Forms.ToolStripMenuItem apartmentPricesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem medianSalariesToolStripMenuItem;
+    }
+}
+
---
+++ statistics-analyzer/MainForm.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace statistics_analyzer
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowError(string message)
+        {
+            MessageBox.Show(message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void UpdateInfo()
+        {
+
+        }
+
+        private void apartmentPricesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void medianSalariesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UpdateTable()
+        {
+
+        }
+
+        private void UpdateChart()
+        {
+
+        }
+
+        private void UpdateSummary()
+        {
+
+        }
+    }
+}
